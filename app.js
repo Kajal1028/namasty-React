09b762cf77dea5f55b3cfe8d876f80710386a108
Kajal1028 <65008189+Kajal1028@users.noreply.github.com>
2024-08-01 19:26:20 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,27 @@
+const heading = React.createElement("h1",
+        {id:"heading"},
+        "Hello world from React! ");
+
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(heading);
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
